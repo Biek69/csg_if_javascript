fff170a4799d023e79c5bbba5792b55c6b5fd294
Biek69 <151120@csgaugustinus.nl>
2020-08-24 11:01:03 +0000

    hrk

--- JS/test.js
+++ JS/test.js
@@ -1,14 +1,17 @@
+function preload(){
+    pl = loadImage("../images/sprites/kever.png");
+}
+
 function setup() {
   canvas = createCanvas(450,450);
   background('silver');
+  textSize(30);
   canvas.parent('processing');
-  //noLoop();
 }
 
-function draw() {
-  noStroke();
-  fill('steelblue');
-  ellipse(0,0,800);
-  fill('deepskyblue');
-  ellipse(450,450,400);
+function draw(){
+    for(var nr = 0; nr < 40000; nr++){
+    image(pl,0,0,50,50);
+    translate(0.01,0)
+    }
 }
