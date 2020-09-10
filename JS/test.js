223c8780641991f8737b527a7ea3ddfa0a3731bb
Biek69 <151120@csgaugustinus.nl>
2020-09-10 14:02:58 +0000

    vet hot

--- JS/test.js
+++ JS/test.js
@@ -1,17 +1,50 @@
-function preload(){
-    pl = loadImage("../images/sprites/kever.png");
-}
+var speed = 20;
+var knop = {
+    x: 500,
+    y: 425,
+    teken() {
+        fill("green");
+        rect(knop['x'], knop['y'], 40, 20);
+    },
+    aanraken() {
+
+    }
+};
+var bal = {
+    X: 250,
+    Y: 250,
+    teken() {
+        fill("white");
+        ellipse(bal['X'], bal['Y'], 75);
+        bal["X"] = constrain(bal["X"], 37.5, 962.5);
+        bal["Y"] = constrain(bal["Y"], 37.5, 812.5);
+    },
+    beweeg() {
+        if (keyIsDown(RIGHT_ARROW)) {
+            this.X += speed;
+        }
+        if (keyIsDown(LEFT_ARROW)) {
+            this.X += -speed;
+        }
+        if (keyIsDown(UP_ARROW)) {
+            this.Y += -speed;
+        }
+        if (keyIsDown(DOWN_ARROW)) {
+            this.Y += speed;
+        }
+    }
+};
 
 function setup() {
-  canvas = createCanvas(450,450);
-  background('silver');
-  textSize(30);
-  canvas.parent('processing');
+    canvas = createCanvas(1000, 850);
+    background('silver');
+    textSize(30);
+    canvas.parent('processing');
 }
 
-function draw(){
-    for(var nr = 0; nr < 40000; nr++){
-    image(pl,0,0,50,50);
-    translate(0.01,0)
-    }
+function draw() {
+    background('silver');
+    bal.beweeg();
+    knop.teken();
+    bal.teken();
 }
