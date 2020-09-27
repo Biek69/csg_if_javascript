614e772416ddddc2f3de75d2f8b37ebaa6d72438
Biek69 <151120@csgaugustinus.nl>
2020-09-27 12:59:56 +0000

    gk

--- JS/test.js
+++ JS/test.js
@@ -4,13 +4,39 @@ var dunte = 800;
 var bal = {
     X: 250,
     Y: 250,
-    hoogte: 80,
+    hoogte: 100,
     breedte: 100,
     teken() {
         fill("white");
         image(boot, bal['X'], bal['Y'], this.breedte, this.hoogte);
-        bal["X"] = constrain(bal["X"], 0, dikte-this.breedte);
-        bal["Y"] = constrain(bal["Y"],  0, dunte-this.hoogte);
+        bal["X"] = constrain(bal["X"], 0, dikte - this.breedte);
+        bal["Y"] = constrain(bal["Y"], 0, dunte - this.hoogte);
+    },
+    beweeg() {
+        if (keyIsDown(RIGHT_ARROW)) {
+            this.X += speed;
+        }
+        if (keyIsDown(LEFT_ARROW)) {
+            this.X += -speed;
+        }
+        if (keyIsDown(UP_ARROW)) {
+            this.Y += -speed;
+        }
+        if (keyIsDown(DOWN_ARROW)) {
+            this.Y += speed;
+        }
+    }
+};
+
+var hitbox = {
+    X: 300,
+    Y: 305,
+    breedte: 100,
+    teken() {
+        ellipse(this.X, this.Y, this.breedte)
+        fill("white");
+        hitbox["X"] = constrain(hitbox["X"], 50, dikte - this.breedte / 2);
+        hitbox["Y"] = constrain(hitbox["Y"], 50, dunte - this.breedte / 2);
     },
     beweeg() {
         if (keyIsDown(RIGHT_ARROW)) {
@@ -36,10 +62,10 @@ var eiland = {
     benGeraakt: false,
 
     wordJeGeraakt(bal) {
-        if (dist(this.x,this.y,bal.X,bal.Y)<=this.breedte/2 + bal.breedte/2) {
+        if (dist(this.x, this.y, hitbox.X, hitbox.Y) <= this.breedte / 2 + hitbox.breedte / 2) {
             this.benGeraakt = true;
         }
-        else{
+        else {
             this.benGeraakt = false;
         }
     },
@@ -51,11 +77,24 @@ var eiland = {
         else {
             fill('green');
         }
-        ellipse(this.x, this.y, this.breedte,);
+        ellipse(this.x, this.y, this.breedte);
     }
 }
-function preload(){
+
+var boompie = {
+    X: 440,
+    Y: 330,
+    breedte: 100,
+    hoogte: 100,
+    teken() {
+        image(palm, boompie['X'], boompie['Y'], this.breedte, this.hoogte);
+    },
+}
+
+function preload() {
+    palm = loadImage('images/palmboom.png');
     boot = loadImage('images/dikkevetteboot.png')
+    steen = loadImage('images/steen.png')
 }
 
 
@@ -72,4 +111,6 @@ function draw() {
     eiland.teken();
     bal.teken();
     eiland.wordJeGeraakt(bal);
+    hitbox.beweeg();
+    boompie.teken();
 }
